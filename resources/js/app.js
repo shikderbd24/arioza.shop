601c309f5811533d7e2397f5d08595c55e557c86
shikderbd24 <shikderplay@gmail.com>
2025-08-09 21:36:20 +0300

    JS fixed

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,30 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const menuIcon = document.getElementById("menu-bars");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuIcon.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+});
+
+// Optional: Hide menu if clicked outside
+document.addEventListener("click", function (e) {
+    if (!menuIcon.contains(e.target) && !mobileMenu.contains(e.target)) {
+        mobileMenu.classList.add("hidden");
+    }
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        menuIcon.classList.add("hidden");
+    } else {
+        menuIcon.classList.remove("hidden");
+    }
+});
+
+mobileMenu.classList.toggle("hidden");
+mobileMenu.classList.toggle("scale-y-100");
+mobileMenu.classList.toggle("scale-y-0");
+
