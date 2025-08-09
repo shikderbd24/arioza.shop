3e650720f1bbed509cd81e8f777bbbb6d345644f
shikderbd24 <shikderplay@gmail.com>
2025-08-09 22:28:30 +0300

    Regsitration Verification Enabled

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/send-test-email', function () {
+    Mail::raw('This is a test email from Laravel SMTP setup.', function ($message) {
+        $message->to(['Mdnajmulhasan619@gmail.com', 'shikderplay@gmail.com'])
+            ->subject('Laravel SMTP Test');
+    });
+
+    return 'Test email sent!';
 });
 
 require __DIR__.'/auth.php';
